6ae99395047be9e50d72c886573936c729c886c6
Filip Palusiński <48621677+FilipPalusinski@users.noreply.github.com>
2021-08-31 18:18:48 +0200

    Initial commit

---
+++ app/src/main/java/pl/studioandroida/gettingstartedwithhilt/MyApplication.kt
@@ -0,0 +1,12 @@
+package pl.studioandroida.gettingstartedwithhilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+
+
+
+}
